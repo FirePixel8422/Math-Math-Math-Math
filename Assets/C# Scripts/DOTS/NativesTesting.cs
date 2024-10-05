ca38ba7577ca2076f2928a8484e115863c17758b
FirePixel8422 <bmtminecraft@hotmail.com>
2024-10-05 16:58:23 +0200

    Burst Parralel MultiJob 2

--- Assets/C# Scripts/DOTS/NativesTesting.cs
+++ Assets/C# Scripts/DOTS/NativesTesting.cs
@@ -1,50 +1,98 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Jobs;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Jobs;
 
 public class NativesTesting : MonoBehaviour
 {
     public int strength;
-    public bool useBurst;
+
+    public int jobsPerFrame = 30;
+    private int debugPrevJobsPerFrame;
+    public int batchSize;
+
+    public int completions;
+    public float elapsed;
+
+    public float timeCheck;
+
+    public bool paused;
+
+
+    private NativeArray<JobHandle> jobHandles;
+
+
+    private void OnValidate()
+    {
+        if (jobsPerFrame != debugPrevJobsPerFrame)
+        {
+            debugPrevJobsPerFrame = jobsPerFrame;
+        }
+    }
+
+
+    private void Start()
+    {
+        debugPrevJobsPerFrame = jobsPerFrame;
+    }
 
     private void Update()
     {
-        Stopwatch sw = Stopwatch.StartNew();
-
-        if (useBurst)
+        if (paused)
         {
-            BurstTestJob burstJob = new BurstTestJob
+            return;
+        }
+
+        int jobsLeft = jobHandles.Length;
+        for (int i = 0; i < jobHandles.Length; i++)
+        {
+            if (jobHandles[i].IsCompleted)
             {
-                strength = strength
-            };
-
-            // Schedule the job in parallel over the number of iterations
-            JobHandle handle = burstJob.Schedule(strength, 12); // 64 is the batch size for splitting the job across threads
-            handle.Complete();  // Wait for job completion
-
-            print(sw.ElapsedMilliseconds + "ms");
+                completions += 1;
+                jobsLeft -= 1;
+            }
         }
-        else
-        {
-            BurstLessNativesTesting();
 
-            print(sw.ElapsedMilliseconds + "ms");
+        if (jobsLeft == 0 && (completions > 0 || paused == false))
+        {
+            if (jobHandles.Length != 0)
+            {
+                JobHandle.CompleteAll(jobHandles);
+            }
+
+            jobHandles.Dispose();
+
+            jobHandles = new NativeArray<JobHandle>(jobsPerFrame, Allocator.Persistent);
+
+
+            for (int i = 0; i < jobsPerFrame; i++)
+            {
+                var burstJob = new BurstTestJob { strength = strength };
+                jobHandles[i] = burstJob.Schedule(strength, batchSize); // Schedule the job
+            }
+        }
+
+        
+        elapsed += Time.deltaTime;
+
+        if (elapsed >= timeCheck)
+        {
+            paused = true;
+
+            elapsed -= timeCheck;
+
+            print($"{completions} tasks completed at a batch size of {batchSize}, with {jobsPerFrame} jobs calls at a time");
+
+            completions = 0;
         }
     }
 
-    public void BurstLessNativesTesting()
-    {
-        float power = 1;
-        for (int i = 0; i < strength; i++)
-        {
-            power *= Mathf.Sqrt(power) + Mathf.Sin(i);
-            power /= Mathf.Sqrt(power) + Mathf.Cos(i);
-        }
-    }
 
     [BurstCompile]
     public struct BurstTestJob : IJobParallelFor
@@ -53,12 +101,20 @@ public class NativesTesting : MonoBehaviour
 
         public void Execute(int index)
         {
-            float power = 1;
+            Int32 value1 = 0;
+
+            // First heavy calculation
             for (int i = 0; i < strength; i++)
             {
-                //power *= Mathf.Sqrt(power) + Mathf.Sin(i);
-                power /= Mathf.Sqrt(power) + Mathf.Cos(i);
-                power = 2 * power;
+                value1 += Mathf.RoundToInt(Mathf.Pow(Mathf.Sin(i * 0.1f), 2) * Mathf.Sqrt(i));
+            }
+
+            Int32 value2 = 0;
+
+            // Second heavy calculation that cancels out the first
+            for (int i = 0; i < strength; i++)
+            {
+                value2 += Mathf.RoundToInt(Mathf.Pow(Mathf.Sin(i * 0.1f + Mathf.PI), 2) * Mathf.Sqrt(i));
             }
         }
     }
